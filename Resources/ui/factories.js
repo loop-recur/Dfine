8ab90229783f6b4b9cc7c8663682014a3b0457c1
Brian <brian@trnsfr.com>
2012-01-12 18:31:08 -0800

    finishing toggles

--- Resources/ui/factories.js
+++ Resources/ui/factories.js
@@ -1,20 +1,51 @@
 UI.toggleableLabel = function(attrs) {
-	return Ti.UI.createLabel(merge({
+	
+	var label = Ti.UI.createLabel(merge({
 		font:{fontFamily:'Helvetica-Light',fontSize:17,fontWeight:'regular'},
 		color:"#023f66",
 		width:'auto',
-		height:'auto',
+		height:'auto'
 	}, attrs));
+	
+	return label;
 }
 
 UI.toggleableView = function(attrs) {
-	return Ti.UI.createView(merge({
-		backgroundColor:"#EDEDED",
-		height:220,
-		width:180,
-		visible:false,
+	var view = Ti.UI.createView(merge({visible:false, width: 200, height: 'auto'}, attrs));
+	
+	var box = Ti.UI.createView({
+		backgroundColor:"#ffffff",
+		backgroundGradient:{
+			type:'radial',
+			colors:['#efefef','#ffffff'],
+			startRadius: '100%',
+			endRadius: "60%",
+			backFillStart:false
+		},
+		borderRadius: 8,
+		borderWidth: 1,
+		borderColor: "#cccccc",
+		height:'100%',
+		width:'100%'
+	});
+		
+	var close_button = Ti.UI.createImageView({
+		image: "images/reveal_close_x.png",
+		width: 30,
+		height: 30,
+		top: -15,
+		right: -15,
 		zIndex: 10
-	}, attrs));
+	});
+
+	close_button.addEventListener('click', function() {
+		view.visible = false;
+	});
+	
+	view.add(box);
+	view.add(close_button);
+
+	return view;
 }
 
 

1564714aa1f470b7c2d64193be03c5182098986d
Taylor Smith <tjsmith19@gmail.com>
2013-01-23 11:05:53 -0800

    last changes

--- Resources/views/content/b_cost2.js
+++ Resources/views/content/b_cost2.js
@@ -1,22 +1,10 @@
 Views.content.b_cost2 = function() {	
 	var view = Views.shared.bg_left_main_view("star");
 	
-	var label = Ti.UI.createLabel({
-		text: "Comparison to standard of care is compelling:",
-		font:{fontFamily:'Helvetica LT CondensedLight',fontSize:50,fontWeight:'regular'},
-		color:"#023f66",
-		width:'auto',
-		top:80,
-		right:40,
-		height:'auto'
-	});
-	
-	view.add(label);
-	
 	var view_content = Ti.UI.createView({
-		backgroundImage:"images/star/acomp2/results8.png",
-		width:722,
-		height:350,
+		backgroundImage:"images/star/acomp2/comparison_page.png",
+		width:1024,
+		height:768,
 	});
 	
 	var back_button = Ti.UI.createButton({
@@ -28,6 +16,17 @@ Views.content.b_cost2 = function() {
 		left:40,
 		zIndex:40
 	});
+	
+	var image = Ti.UI.createView({
+		backgroundImage:"images/star/acomp2/comparison_chart.png",
+		top: 160,
+		width:722,
+		height:500,
+	});
+	
+	view_content.add(image);
+	
+	ClickToExpand(image);
 
 	back_button.addEventListener('click', function(e){Controllers.content.renderView("b_tabs", "b_cost")});
 	view.add(back_button);
